25be918737efc6d6597457a0c401fdbda4417b6c
Kelley-Nocturnal <87814010+Kelley-Nocturnal@users.noreply.github.com>
2022-12-03 10:24:08 -0500

    Solved Day 0 part 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,33 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCals: Int = 0
+        var subTotal: Int = 0
+        var calorieList: MutableList<Int> = mutableListOf()
+     input.forEachIndexed { i, s ->
+         if (i < input.size-1) {
+             if (s != "") {
+                 subTotal += s.toInt()
+             } else {
+                 calorieList.add(subTotal)
+                 subTotal = 0
+             }
+         }
+         else{
+             subTotal += s.toInt()
+             calorieList.add(subTotal)
+         }
+     }
+        maxCals = calorieList.max()
+        return maxCals
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+
 }
+
