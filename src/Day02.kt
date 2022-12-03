46dc0809766455f0d94f51b54b44802bee70a4b9
kfbower <88797086+kfbower@users.noreply.github.com>
2022-12-03 13:21:47 -0500

    Day 2 part 2 solved
    Signed-off-by: Kelley-Nocturnal <87814010+Kelley-Nocturnal@users.noreply.github.com>

--- src/Day02.kt
+++ src/Day02.kt
@@ -29,8 +29,60 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
+        //(1 for Rock, 2 for Paper, and 3 for Scissors)
+        //X means you need to lose, Y means you need to end the round in a draw, and Z means you need to win.
+        val oppRock = "A"
+        val oppPaper = "B"
+        val oppScissor = "C"
+        val win = "Z"
+        val lose = "X"
+        val draw = "Y"
+        var matchScore: Int = 0
+        var shapeScore: Int = 0
+        var totalScore: Int = 0
+        input.forEachIndexed { i, s ->
+            if (s[0].toString() == oppRock && s[2].toString() == win) {
+                shapeScore+=2
 
-   return input.size
+            } else if (s[0].toString() == oppRock && s[2].toString() == draw)  {
+                shapeScore+=1
+
+            } else if (s[0].toString() == oppRock && s[2].toString() == lose)  {
+                shapeScore+=3
+            }
+            else if (s[0].toString() == oppPaper && s[2].toString() == win) {
+                shapeScore+=3
+
+            } else if (s[0].toString() == oppPaper && s[2].toString() == draw)  {
+                shapeScore+=2
+
+            } else if (s[0].toString() == oppPaper && s[2].toString() == lose)  {
+                shapeScore+=1
+            }
+            else if (s[0].toString() == oppScissor && s[2].toString() == win) {
+                shapeScore+=1
+
+            } else if (s[0].toString() == oppScissor && s[2].toString() == draw)  {
+                shapeScore+=3
+
+            } else if (s[0].toString() == oppScissor && s[2].toString() == lose)  {
+                shapeScore+=2
+            }
+        }
+        input.forEachIndexed { i, s ->
+            if (s[2].toString() == "X") {
+                matchScore += 0
+            } else if (s[2].toString() == "Y") {
+                matchScore += 3
+            } else if (s[2].toString() == "Z") {
+                matchScore += 6
+            }
+        }
+        totalScore = matchScore+shapeScore
+
+        return totalScore
+
+
     }
 
 
