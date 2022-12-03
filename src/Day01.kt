523c41e4fb6f5aa4670246f5f4613afb6a5009df
kfbower <88797086+kfbower@users.noreply.github.com>
2022-12-03 11:02:18 -0500

    Solved Day 0 part 2
    Signed-off-by: Kelley-Nocturnal <87814010+Kelley-Nocturnal@users.noreply.github.com>

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,34 +1,52 @@
 fun main() {
+
+    fun makeSummedList(input: List<String>): MutableList<Int> {
+        var calorieList: MutableList<Int> = mutableListOf()
+        var subTotal: Int = 0
+        input.forEachIndexed { i, s ->
+            if (i < input.size-1) {
+                if (s != "") {
+                    subTotal += s.toInt()
+                } else {
+                    calorieList.add(subTotal)
+                    subTotal = 0
+                }
+            }
+            else{
+                subTotal += s.toInt()
+                calorieList.add(subTotal)
+            }
+        }
+        return calorieList
+    }
     fun part1(input: List<String>): Int {
         var maxCals: Int = 0
-        var subTotal: Int = 0
-        var calorieList: MutableList<Int> = mutableListOf()
-     input.forEachIndexed { i, s ->
-         if (i < input.size-1) {
-             if (s != "") {
-                 subTotal += s.toInt()
-             } else {
-                 calorieList.add(subTotal)
-                 subTotal = 0
-             }
-         }
-         else{
-             subTotal += s.toInt()
-             calorieList.add(subTotal)
-         }
-     }
+        val calorieList = makeSummedList(input)
         maxCals = calorieList.max()
         return maxCals
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val calorieList = makeSummedList(input)
+        val calListSorted = calorieList.sorted()
+        println(calListSorted)
+
+        var top1 = calListSorted[calListSorted.size-1]
+        println(top1)
+        var top2 = calListSorted[calListSorted.size-2]
+        println(top2)
+        var top3 = calListSorted[calListSorted.size-3]
+        println(top3)
+        var totalTop3 = top1+top2+top3
+
+
+        return totalTop3
     }
 
 
     val input = readInput("Day01")
     println(part1(input))
-    ///
+    println(part2(input))
 
 }
 
