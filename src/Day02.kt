e811cf1f3504cea14348d9352d46eba005fe34f8
kfbower <88797086+kfbower@users.noreply.github.com>
2022-12-03 11:11:33 -0500

    Day 2 part 1 solved

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,7 +1,6 @@
 fun main() {
 
     fun part1(input: List<String>): Int {
-
         var matchScore: Int = 0
         var shapeScore: Int = 0
         var totalScore: Int = 0
@@ -26,6 +25,7 @@ fun main() {
            totalScore = matchScore+shapeScore
 
         return totalScore
+
     }
 
     fun part2(input: List<String>): Int {
