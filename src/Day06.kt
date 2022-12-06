6c0c50503206728e96782a6fef75449bab6ee879
Kelley-Nocturnal <87814010+Kelley-Nocturnal@users.noreply.github.com>
2022-12-06 09:36:14 -0500

    Day 6

---
+++ src/Day06.kt
@@ -0,0 +1,55 @@
+fun main() {
+    fun part1(input: List<String>): Int{
+        var finalIndex = 0
+        var char1: Char?
+        var char2: Char?
+        var char3: Char?
+        var char4: Char?
+        val charset: MutableList<Char> = mutableListOf()
+
+        input.forEachIndexed { i, s ->
+
+            var j=0
+
+            while (j< s.length){
+                char1 = s[j]
+                char2 = s[j+1]
+                char3 = s[j+2]
+                char4 = s[j+3]
+                charset += char1!!
+                charset += char2!!
+                charset += char3!!
+                charset += char4!!
+                println(charset.toString())
+
+               val distinctcount =  charset.distinct().count()
+
+                if (distinctcount <4){
+                    j++
+                    char1=null
+                    char2=null
+                    char3=null
+                    char4=null
+                    charset.clear()
+                }
+                else {
+                    j+=4
+                    finalIndex = j
+                    return@forEachIndexed
+
+                }
+            }
+        return finalIndex
+
+
+        }
+        return finalIndex
+    }
+    fun part2(input: List<String>): Int{
+        return input.size
+    }
+
+    val input = readInput("Day06")
+    println(part1(input))
+    println(part2(input))
+}
